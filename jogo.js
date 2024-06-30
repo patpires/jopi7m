a033998e538ba920a9867259e5219a9304706f2a
patpires <patpires@gmail.com>
2024-06-30 12:16:52 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -81,10 +81,10 @@ function criaChao() {
 }
 
 function fazColisao(Joaninha, chao) {
-  const Joaninha = Joaninha.y + Joaninha.altura;
+  const Joaninha1 = Joaninha1.y + Joaninha1.altura;
   const chaoY = chao.y;
 
-  if(Joaninha >= chaoY) {
+  if(Joaninha1 >= chaoY) {
     return true;
   }
 
